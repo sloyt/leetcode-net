7baefc8f4eeece16a252fc275b58c8007c738723
sloyt <slo.yt@yandex.ru>
2025-03-23 18:58:58 -0400

    task 1976 (Dijkstra)

--- leetcode-net/Problem/p1976/Solution.cs
+++ leetcode-net/Problem/p1976/Solution.cs
@@ -2,94 +2,61 @@ namespace leetcode_net.Problem.p1976;
 
 public class Solution
 {
-    public readonly int Divider = (int)Math.Pow(10, 9) + 7; 
+    public readonly int Divider = (int)Math.Pow(10, 9) + 7;
     
     public int CountPaths(int n, int[][] roads) {
         //
         // build graph
         
-        int[][][] graph = new int[n][][];
-
-        foreach (int[] road in roads)
-        {
-            if (graph[road[0]] == default)
-            {
-                graph[road[0]] = [[road[1], road[2]]];
-            }
-            else
-            {
-                Array.Resize(ref graph[road[0]], graph[road[0]].Length + 1);
-                graph[road[0]][graph[road[0]].Length - 1] = [road[1], road[2]];
-            }
-            
-            if (graph[road[1]] == default)
-            {
-                graph[road[1]] = [[road[0], road[2]]];
-            }
-            else
-            {
-                Array.Resize(ref graph[road[1]], graph[road[1]].Length + 1);
-                graph[road[1]][graph[road[1]].Length - 1] = [road[0], road[2]];
-            }
-        }
-
-        // prepare variables
-
-        int pathCount = 0;
-        long maxTime = long.MaxValue;
-
-        var visited = new int[n];
-
-        // dfs algorithm
+        List<List<(int node, long time)>> graph = new List<List<(int, long)>>();
         
-        void Dfs(int nodeIndex, long time)
-        {
-            visited[nodeIndex] = 1;
+        for (int i = 0; i < n; i++) {
+            graph.Add(new List<(int, long)>());
+        }
+        
+        foreach (var road in roads) {
+            graph[road[0]].Add((road[1], road[2]));
+            graph[road[1]].Add((road[0], road[2]));
+        }
+        
+        // prepare values
+        
+        var bestTimes = new long[n];
+        var repeats = new int[n];
+        var queue = new PriorityQueue<(int node, long time), long>();
+        
+        // Dijkstra's algorithm
+
+        repeats[0] = 1;
+        queue.Enqueue((0, 0), 0);
+        
+        while (queue.Count > 0) {
+            var (node, time) = queue.Dequeue();
             
-            // go deeper
-
-            foreach (int[] neighbor in graph[nodeIndex])
+            // if there is shorter path, skip
+            if (time > bestTimes[node]) continue;
+            
+            // process all neighbors
+            foreach (var (nextNode, nextTime) in graph[node])
             {
-                if (visited[neighbor[0]] == 0)
+                long newTime = time + nextTime;
+
+                // check whether we found a new shortest path
+                if (bestTimes[nextNode] == 0 || newTime < bestTimes[nextNode])
                 {
-                    // check whether it is direct connection to final point
-
-                    if (neighbor[0] == n - 1)
-                    {
-                        // check timing to final point
-
-                        if (time + neighbor[1] == maxTime)
-                        {
-                            pathCount = (pathCount + 1) % Divider;
-                        }
-                        else if (time + neighbor[1] < maxTime)
-                        {
-                            maxTime = time + neighbor[1];
-                            pathCount = 1;
-                        }
-                    }
-                    else
-                    {
-                        // it is not direct connection
-                        // check time would be less than maxTime
-                        if (time + neighbor[1] < maxTime)
-                        {
-                            Dfs(neighbor[0], time + neighbor[1]);
-                        }
-                    }
+                    bestTimes[nextNode] = newTime;
+                    repeats[nextNode] = repeats[node];
+                    queue.Enqueue((nextNode, newTime), newTime);
+                }
+                else if (newTime == bestTimes[nextNode])
+                {
+                    repeats[nextNode] = (repeats[nextNode] + repeats[node]) % Divider;
                 }
             }
-
-            visited[nodeIndex] = 0;
         }
-
-        // run dfs
-        
-        visited[0] = 1;
-        Dfs(0, 0);
         
         // end
-
-        return pathCount;
+        
+        return repeats[n - 1];
     }
 }
--- leetcode-net/Program.cs
+++ leetcode-net/Program.cs
@@ -1,7 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Diagnostics;
+
 Console.WriteLine("I'm here");
 
 // var solution = new Solution();
+var stopWatch = new Stopwatch();
 
 // code here to debug solution is working
